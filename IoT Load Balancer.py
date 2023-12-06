7ff8228ac5903bb3980af45b53f3e51492cae856
ujjwal-malik-um <152059523+ujjwal-malik-um@users.noreply.github.com>
2023-12-06 12:24:03 +0530

    Update IoT Load Balancer.py

--- IoT Load Balancer.py
+++ IoT Load Balancer.py
@@ -28,17 +28,24 @@ class System:
                 count +=  1
         return count
     
-    def OverloadNodeHelper(self, l, r):
-        if l == r:
-            return self.master_node_list[l]
-        mid = (l + r) // 2
-        left_overloaded = self.SensorAssignedCount(l, mid, self.master_node_list[mid])
-        right_overloaded = self.SensorAssignedCount(mid + 1, r, self.master_node_list[mid])
-        if left_overloaded:
-            return self.OverloadNodeHelper(l, mid)
-        elif right_overloaded:
-            return self.OverloadNodeHelper(mid + 1, r)
-        return None
+    def OverloadNodeHelper(self, l, r):        
+        if (l == r):
+            return self.sensor_mapping_list[l]
+            
+        mid = (r + l)//2
+        lOverloadSensor = self.OverloadNodeHelper (l, mid)
+        rOverloadSensor = self.OverloadNodeHelper (mid + 1, r)
+        
+        if (lOverloadSensor == rOverloadSensor):
+            return lOverloadSensor
+                
+        lCount = self.SensorAssignedCount(self.sensor_mapping_list, l, r, lOverloadSensor)
+        rCount = self.SensorAssignedCount(self.sensor_mapping_list, l, r, rOverloadSensor)
+        
+        if (lCount>rCount):
+            return lOverloadSensor
+        else: 
+            return rOverloadSensor
         
     def getOverloadedNode(self):
         return self.OverloadNodeHelper(0, len(self.sensor_mapping_list)-1)
@@ -57,32 +64,8 @@ class System:
 if __name__ == "__main__":
     test_system1 = System()
     
-    test_system1.config_system('app_data1.csv')
+    test_system1.config_system('app_data.csv')
     
     print("Overloded Master Node : ", test_system1.getOverloadedNode())
     
     print("Partially Overloaded Master Node : ", test_system1.getPotentialOverloadNode())
-
-    test_system2 = System()
-    
-    test_system2.config_system('app_data2.csv')
-    
-    print("Overloded Master Node : ", test_system2.getOverloadedNode())
-    
-    print("Partially Overloaded Master Node : ", test_system2.getPotentialOverloadNode())
-
-    test_system3 = System()
-
-    test_system3.config_system('app_data3.csv')
-    
-    print("Overloded Master Node : ", test_system3.getOverloadedNode())
-    
-    print("Partially Overloaded Master Node : ", test_system3.getPotentialOverloadNode())
-
-    test_system4 = System()
-
-    test_system4.config_system('app_data4.csv')
-    
-    print("Overloded Master Node : ", test_system4.getOverloadedNode())
-    
-    print("Partially Overloaded Master Node : ", test_system4.getPotentialOverloadNode())
